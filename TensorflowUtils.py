4dc4b44abd990af177c5c0634c5ec1b10beb2737
黄 枭 <xiao.huang@ruisdata.com>
2019-03-06 10:17:28 +0800

    first commit

--- TensorflowUtils.py
+++ TensorflowUtils.py
@@ -63,26 +63,30 @@ def get_variable(weights, name):
     return var
 
 
-def weight_variable(shape, stddev=0.02, name=None):
-    # print(shape)
+# def weight_variable(shape, stddev=0.02, name=None):
+#     # print(shape)
+#     initial = tf.truncated_normal(shape, stddev=stddev)
+#     if name is None:
+#         return tf.Variable(initial)
+#     else:
+#         return tf.get_variable(name, initializer=initial)
+def weight_variable(shape, stddev=0.1, name="weight"):
     initial = tf.truncated_normal(shape, stddev=stddev)
-    if name is None:
-        return tf.Variable(initial)
-    else:
-        return tf.get_variable(name, initializer=initial)
+    return tf.Variable(initial, name=name)
 
+# def bias_variable(shape, name=None):
+#     initial = tf.constant(0.0, shape=shape)
+#     if name is None:
+#         return tf.Variable(initial)
+#     else:
+#         return tf.get_variable(name, initializer=initial)
+def bias_variable(shape, name="bias"):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial, name=name)
 
-def bias_variable(shape, name=None):
-    initial = tf.constant(0.0, shape=shape)
-    if name is None:
-        return tf.Variable(initial)
-    else:
-        return tf.get_variable(name, initializer=initial)
-
-
-def get_tensor_size(tensor):
-    from operator import mul
-    return reduce(mul, (d.value for d in tensor.get_shape()), 1)
+# def get_tensor_size(tensor):
+#     from operator import mul
+#     return reduce(mul, (d.value for d in tensor.get_shape()), 1)
 
 
 def conv2d_basic(x, W, bias):
@@ -111,6 +115,11 @@ def conv2d_transpose_strided(x, W, b, output_shape=None, stride = 2):
 def leaky_relu(x, alpha=0.0, name=""):
     return tf.maximum(alpha * x, x, name)
 
+def deconv2d(x, W,stride): # 固定让反卷积后的尺寸变大两倍
+    with tf.name_scope("deconv2d"):
+        x_shape = tf.shape(x)
+        output_shape = tf.stack([x_shape[0], x_shape[1]*2, x_shape[2]*2, x_shape[3]//2])
+        return tf.nn.conv2d_transpose(x, W, output_shape, strides=[1, stride, stride, 1], padding='VALID', name="conv2d_transpose")
 
 def max_pool_2x2(x):
     return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
@@ -237,3 +246,14 @@ def add_activation_summary(var):
 def add_gradient_summary(grad, var):
     if grad is not None:
         tf.summary.histogram(var.op.name + "/gradient", grad)
+
+
+def crop_and_concat(x1,x2):
+    with tf.name_scope("crop_and_concat"):
+        x1_shape = tf.shape(x1)
+        x2_shape = tf.shape(x2)
+        # offsets for the top left corner of the crop
+        offsets = [0, (x1_shape[1] - x2_shape[1]) // 2, (x1_shape[2] - x2_shape[2]) // 2, 0]
+        size = [-1, x2_shape[1], x2_shape[2], -1]
+        x1_crop = tf.slice(x1, offsets, size)
+        return tf.concat([x1_crop, x2], 3) # 接在屁股后面，通道数变多
